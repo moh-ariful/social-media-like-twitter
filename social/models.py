1965c0b44f6f67b93e2603e12e2c0ee21b4c113a
Moh Ariful <nextkoding@gmail.com>
2022-08-13 16:22:58 +0700

    Selesai Part 6

--- social/models.py
+++ social/models.py
@@ -27,6 +27,8 @@ class UserProfile(models.Model):
     location = models.CharField(max_length=100, blank=True, null=True)
     picture = models.ImageField(upload_to='uploads/profile_pictures',
                                 default='uploads/profile_pictures/default.png', blank=True)
+    followers = models.ManyToManyField(
+        User, blank=True, related_name='followers')
 
 
 @receiver(post_save, sender=User)
--- social/urls.py
+++ social/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import PostListView, PostDetailView, PostEditView, PostDeleteView, CommentDeleteView, ProfileView, ProfileEditView
+from .views import PostListView, PostDetailView, PostEditView, PostDeleteView, CommentDeleteView, ProfileView, ProfileEditView, AddFollower, RemoveFollower
 
 urlpatterns = [
     path('', PostListView.as_view(), name='post-list'),
@@ -10,4 +10,9 @@ urlpatterns = [
          CommentDeleteView.as_view(), name='comment-delete'),
     path('profile/<int:pk>/', ProfileView.as_view(), name='profile'),
     path('profile/edit/<int:pk>/', ProfileEditView.as_view(), name='profile-edit'),
+    path('profile/<int:pk>/followers/add',
+         AddFollower.as_view(), name='add-follower'),
+    path('profile/<int:pk>/followers/remove',
+         RemoveFollower.as_view(), name='remove-follower'),
+
 ]
