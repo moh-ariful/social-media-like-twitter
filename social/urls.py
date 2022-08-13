e6f907f9408175489995781fe40913c882bad928
Moh Ariful <nextkoding@gmail.com>
2022-08-13 13:31:35 +0700

    selesai PART 5

--- social/urls.py
+++ social/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
-from .views import PostListView, PostDetailView, PostEditView, PostDeleteView, CommentDeleteView
+from .views import PostListView, PostDetailView, PostEditView, PostDeleteView, CommentDeleteView, ProfileView, ProfileEditView
 
 urlpatterns = [
     path('', PostListView.as_view(), name='post-list'),
-    path('post/<int:pk>', PostDetailView.as_view(), name='post-detail'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('post/edit/<int:pk>/', PostEditView.as_view(), name='post-edit'),
     path('post/delete/<int:pk>/', PostDeleteView.as_view(), name='post-delete'),
     path('post/<int:post_pk>/comment/delete/<int:pk>/',
          CommentDeleteView.as_view(), name='comment-delete'),
+    path('profile/<int:pk>/', ProfileView.as_view(), name='profile'),
+    path('profile/edit/<int:pk>/', ProfileEditView.as_view(), name='profile-edit'),
 ]
