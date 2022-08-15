714bc31cdbe2cf8234124e18554f605cb6e69ba3
Moh Ariful <nextkoding@gmail.com>
2022-08-15 15:27:31 +0700

    First commit ready to push

--- socialnetwork/settings.py
+++ socialnetwork/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-sakp^-1c)97ewakhm@i-@8z3$80xteobz^4%qbf^gi^8#_)u+i'
+SECRET_KEY = 'django-insecure-sakp^-1c)97ewakhm@i-@8xxxxxxxxxxx^4%qbf^gi^8#_)u+i'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
